514bba7332a415c5d9569b822be26dfc4cd38ab4
El Cascarudo <elcascarudo.dev@gmail.com>
2021-12-03 09:18:02 -0300

    Promer commit

---
+++ src/main/java/com/nttdata/preguntados/PreguntadosApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.preguntados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreguntadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreguntadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/preguntados/PreguntadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.preguntados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
